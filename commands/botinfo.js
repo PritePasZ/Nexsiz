234fe4b5400488d7e76d6ed28d85723ae6a9b607
PritePasZ <bassky2512@gmail.com>
2019-03-06 11:35:05 +0700

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,19 +1,21 @@
-const Discord = require("discord.js")
-const bot = new Discord.Client
-module.exports.run = async (bot, message, args) => {
-  let boticon = bot.user.displayAvatarURL;
-    let botembed = new Discord.RichEmbed()
-    .setDescription("Bot Information")
-    setColor("#e51212")
-    .setThumbnail(boticon)
-    .addField("Bot Name", bot.user.username)
-    .addField("Bot Create Date", bot.user.createAt)
-    .addField("Servers", bot.guilds.size)
+const Discord = require("discord.js");
 
-    message.channel.send(botembed)
+module.exports.run = async (bot, message, args) => {
+
+  let serverembed = new Discord.RichEmbed()
+  .setTitle("Server Information")
+  .setColor("0ED4DA")
+  .setThumbnail(message.guild.iconURL)
+  .addField('Name', `${message.guild.name} (${message.guild.nameAcronym})`, true)
+  .addField('Server Owner', message.guild.owner.user.tag, true)
+  .addField("Server Create Date", message.guild.createdAt, true)
+  .addField("Member Count", message.guild.memberCount, true)
+
+  return message.channel.send(serverembed);
 }
 
+
 module.exports.help = {
-  name: "botinfo",
-  description: "Show the bot's info."
+  name: "serverinfo",
+  description: "Gathers information about the server."
 }
