4ace48af9d5ec383a058184c08c51ee5aaed1a11
PritePasZ <bassky2512@gmail.com>
2019-03-06 15:03:55 +0700

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
   .addField(":bar_chart: Toal Members", message.guild.memberCount, true
   .addField("Member Count", message.guild.memberCount, true)
   .addField(":card_index: Server ID", message.guild.id, true)
-  .addField(":earth_asia:Server Region",message.guild.region, true)
+  .addField(":earth_asia: Server Region",message.guild.region, true)
   .addField(":calendar: Server Create Date", message.guild.createdAt, true)
 
   return message.channel.send(serverembed);
