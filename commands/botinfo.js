a695b5e76b492d939358953db1e44906413be7c2
PritePasZ <bassky2512@gmail.com>
2019-03-06 12:57:27 +0700

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
   .addField(":calendar: Server Create Date", message.guild.createdAt, true)
   .addField(":bar_chart: Toal Members", message.guild.memberCount, true)
   .addField("Member Count", message.guild.memberCount, true)
-  .addField("Bot Count", message.guild.botCount, true)
+  .addField("Bot Count", message.guild.bot.memberCount, true)
   .addField(":card_index: Server ID", message.guild.id, true)
   .addField(":earth_asia:Server Region",message.guild.region, true)
 
