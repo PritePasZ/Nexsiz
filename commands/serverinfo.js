b3ddf92d7b6cf37bf4917f9d932f3a1cfb9a8007
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-03-06 18:19:42 +0700

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -13,6 +13,7 @@ module.exports.run = async (bot, message, args) => {
   .addField(":earth_asia: Server Region", message.guild.region, true)
   .addField(":bar_chart: Total Member", message.guild.memberCount, true)
   .addField("Member Count", message.guild.memberCount, true)
+  .addField("Bot Count", message.guild.members.filter(m => m.user.bot).size}\``, true)
   .addField(":calendar: Server Create Date", message.guild.createdAt, true)
 
   return message.channel.send(serverembed);
