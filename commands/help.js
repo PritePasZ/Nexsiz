9d5e5ae21d90a8f2aed545204d524c04db4eee26
PritePasZ <bassky2512@gmail.com>
2019-03-06 10:56:06 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -12,8 +12,7 @@ let dm = new Discord.RichEmbed()
 let helpembed = new Discord.RichEmbed()
     .setColor(`#409cd9`)
     .setAuthor(`Commands & Guides`, bot.user.avatarURL)
-    .addField(`:desktop: General Commands`, `--help - Show a list of commands & guides to your DM.\n--ping or --pong - Show the current bot ping/ms.\n--serverinfo - shows the information about the server. [Coming Soon]\n--botinfo - shows the information about the bot. [Coming Soon]
-      --test - test`)
+    .addField(`:desktop: General Commands`, `--help - Show a list of commands & guides to your DM.\n--ping or --pong - Show the current bot ping/ms.]`)
     .setTimestamp()
     message.author.send(helpembed);
 
