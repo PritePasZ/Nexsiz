483ca46e511eb7b85278455f3124ec4cfe87f5a3
PritePasZ <bassky2512@gmail.com>
2019-03-06 10:54:17 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,13 +8,14 @@ let dm = new Discord.RichEmbed()
     .setDescription(`:mailbox_with_mail: I have private messaged you a list of commands!`)
     .setTimestamp()
     message.channel.send(dm);
-    
+
 let helpembed = new Discord.RichEmbed()
     .setColor(`#409cd9`)
     .setAuthor(`Commands & Guides`, bot.user.avatarURL)
-    .addField(`:desktop: General Commands`, `--help - Show a list of commands & guides to your DM.\n--ping or --pong - Show the current bot ping/ms.\n--serverinfo - shows the information about the server. [Coming Soon]\n--botinfo - shows the information about the bot. [Coming Soon]`)
+    .addField(`:desktop: General Commands`, `--help - Show a list of commands & guides to your DM.\n--ping or --pong - Show the current bot ping/ms.\n--serverinfo - shows the information about the server. [Coming Soon]\n--botinfo - shows the information about the bot. [Coming Soon]
+      --test - test`)
     .setTimestamp()
-    message.author.send(helpembed);  
+    message.author.send(helpembed);
 
 }
 module.exports.help = {
