7e87c43d47df3c6f9102fa7c4e248b822553d2cb
PritePasZ <bassky2512@gmail.com>
2019-03-06 14:23:33 +0700

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const bot = new Discord.Client
 
 module.exports.run = async (bot, message, args) => {
+  
     let boicon = bot.user.displayAvatarURL;
     let botembed = new Discord.RichEmbed()
     .setAuthor(message.author.bot.username, message.author.displayAvatarURL)
