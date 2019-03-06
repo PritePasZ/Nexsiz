e8bd0008b9d4bfa3822d861e10e5f41e31ca4832
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-03-06 17:34:12 +0700

    Create serverinfo.js

---
+++ commands/serverinfo.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  let serverembed = new Discord.RichEmbed()
+  .setTitle("Server Information")
+  .setColor("0ED4DA")
+  .setThumbnail(message.guild.iconURL)
+  .setAuthor(message.author.username, message.author.displayAvatarURL)
+  .addField('Server Name', message.guild.name, true)
+  .addField('Server Owner', message.guild.owner.user.tag, true)
+  .addField("Server ID", message.guild.id, true)
+  .addField("Server Region", message.guild.region, true)
+  .addField("Total Member", message.guild.memberCount, true)
+  .addField("Member Count", message.guild.memberCount, true)
+  .addField("Server Create Date", message.guild.createdAt, true)
+  .setFooter("📆 You Joined", message.meber.joinedAt, true)
+
+  return message.channel.send(serverembed);
+}
+
+
+module.exports.help = {
+  name: "serverinfo",
+  description: "Gathers information about the server."
+}
