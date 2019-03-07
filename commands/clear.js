b059b03265e0c7db596ec0651060d522d5cc294b
PritePasZ <bassky2512@gmail.com>
2019-03-07 14:12:42 +0700

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -5,7 +5,6 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
   if(!args[0]) return message.channel.send("no");
   message.channel.bulkDelete(args[0]).then(() => {
- .setColor(`#f4d442`)
   message.channel.send(`:wastebasket: Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
 });
 
