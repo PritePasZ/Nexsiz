2ca219bec7dd10a8c6834e09c2aebd17468ddf54
PritePasZ <bassky2512@gmail.com>
2019-03-07 13:11:56 +0700

    Create botinfo.js

---
+++ commands/botinfo.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client
+
+module.exports.run = async (bot, message, args) => {
+
+    let boticon = bot.user.displayAvatarURL;
+    let botembed = new Discord.RichEmbed()
+    .setDescription("Bot Information")
+    .setColor("0ED4DA")
+    .setThumbnail(boticon)
+    .addField("Bot Name", bot.user.username)
+    .addField("Bot Create Date", bot.user.createdAt)
+
+    message.channel.send(botembed)
+}
+
+module.exports.help = {
+    name: "botinfo",
+    description: "Shows the bot's info."
