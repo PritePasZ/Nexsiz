0bed2af633831a8608ed57b082d3a1baed2398c7
PritePasZ <bassky2512@gmail.com>
2019-03-07 11:46:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ fs.readdir("./commands/", (err, files) => {
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity(`test | --help`);
+  bot.user.setActivity(`with PritePasZ | --help`);
 });
 
 /*bot.on('guildMemberAdd', member => {
