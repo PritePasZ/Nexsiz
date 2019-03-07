02dc00100cad86ce52d371125773ab256f71ded6
PritePasZ <bassky2512@gmail.com>
2019-03-07 13:00:40 +0700

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,29 +1,24 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client
 
 module.exports.run = async (bot, message, args) => {
-
-    let boticon = bot.user.displayAvatarURL;
-    let bname =  bot.user.username;
+    let bicon = bot.user.displayAvatarURL;
+    let bname = bot.user.username;
     let botembed = new Discord.RichEmbed()
-    .setDescription("Bot Information")
-    .setColor("#f7e73b")
-    .setThumbnails(boticon)
-    .setURL("https://discord.gg/P96Pr33")
+    .setColor("#f4d742")
+    .setThumbnail(bicon)
+    .setAuthor(bname, bicon)
+    .setURL(`https://discord.gg/P96Pr33`)
     .setTimestamp()
-    .setTitle("Click here to join PritePasZ discord server")
+    .setTitle(`Click here to join PritePasZ discord server`)
     .setDescription("<:nodejs:553086690105753610> Made with Node.js and Discord.js")
-    .addField("Bot Name", bot.user.username)
-    .addField("Bot Created Date", bot.user.createdAt)
-    .addField("Server", bot.guild.size)
-    .addField(":clipboard: Created By", "PritePasZ#3071", true)
-    .addField(":information source: Version", "Dont have any version yet", true)
-    .addField(":tools Debug", "creator are programming the bot,bot now dont have a version");
-//test typing here 
+    .addField(`:clipboard: Created by`, `PritePasZ`, true)
+    .addField(`:information_source: Version`, `0.0.9`, true)
+    .addField(`:tools: Debug`, `Currency version now is 0.0.9`);
+    .setFooter(`Made by PritePasZ#3071 | Made with Node.js and Discord.js`)
+
     message.channel.send(botembed);
 }
 
 module.exports.help = {
   name: "botinfo"
-  description: "Show the bot's info"
 }
