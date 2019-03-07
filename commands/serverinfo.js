c8a07c2952d817c223d28935cf44dd8dc53280d8
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-03-07 20:18:24 +0700

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -8,13 +8,13 @@ module.exports.run = async (bot, message, args) => {
   .setThumbnail(message.guild.iconURL)
   .setAuthor(message.author.username, message.author.displayAvatarURL)
   .addField('`:white_check_mark: \`Server Name\``, `\`${message.guild.name}\``, true)
-  .addField(`<:Owner:553078012283715636> \`Server Owner\``, `\`${message.guild.owner.user.tag}\``, true)
+  .addField(`<:Owner:553078012283715636> \`Owner\``, `\`${message.guild.owner.user.tag}\``, true
   .addField(`:card_index: \`Server ID\``, `\`${message.guild.id}\``, true)
-  .addField(`:earth_asia: \`Server Region\``, `\`${message.guild.region}\``, true)
-  .addField(`:bar_chart: \`Total Member\``, `\`${message.guild.memberCount}\``, true)
+  .addField(`:earth_asia: \`Region\``, `\`${message.guild.region}\``, true)
+  .addField(`:bar_chart: \`Total Members\``, `\`${message.guild.memberCount}\``)
   .addField(`<:bustinsilhouette:553086092837126145> \`Member Count\``, `\`${message.guild.memberCount\``, true)
   .addField(`:robot: \`Bot Count\``, `\`${message.guild.members.filter(m => m.user.bot).size}``, true))}
-  .addField(`clipboard: \`Joined Date\``, `\`${message.guild.joinedAt}\``, true)
+  .addField(`:clip_board:\`Join Date\``, `\`${message.member.joinedAt}\``, true)
   .addField(`:calendar: \`Server Create Date\``, `\`${message.guild.createdAt}\``, true);
 
   message.channel.send(serverembed);
