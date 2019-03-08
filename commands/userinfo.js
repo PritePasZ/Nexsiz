aef055a86f243233ef94a2b2d8af514503648c08
PritePasZ <bassky2512@gmail.com>
2019-03-08 15:01:55 +0700

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,32 +1,28 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
-  let user;
-              //test typing here
-    if (messsage.mentions.user.first()) {
-      user = message.mentions.users.first();
+    let user;
+    if (message.mentions.user.first())
+        user = message.mentions.users.first();
+
     } else {
         user = message.author;
     }
 
     const member = message.guild.member(user);
 
-
     const embed = new Discord.RichEmbed()
-                .setColor("#f44242")
-                .setThumbnail(user.avatarURL)
-                .setAuthor(`${user.username}`, user.avatarURL)
-  .addField("<:bustinsilhouette:553086092837126145> Username", user.username#user.discriminator, true)
-  .addField(":information_source: User ID", user.id, true)
-               .addField(":card_index: Nickname", member.nickname !== null ? member.nickname : 'None', true)
-               .addField(":robot: Bot Account", user.bot, true)
-               .addField("<:online:553477759863619604> User Stat", user.presence.game.name : 'None', true)
-               .addField(":joystick: User Activity", user.presence.game ? user.presence.game.name : 'None')
-               .addField(":pushpin: Roles", member.roles.map(role => `<@&${roles.id}>`).join(', '), true)
-  .addField(":calendar: Joined Guild Date", member.joinedAt, true)
-  .addField(":calendar: Account Created Date", user.createdAt, true)
-  .setFooter(`Shows the information of user, ${message.author.username}#${message.author.discriminator}`)
-  message.channel.send({embed});
+    .setColor("#4bf442")
+    .setThumbnail(message.author.avatarURL)
+    .setTitle(`${user.username}#${user.discriminator}`)
+    .addField(":card_index: Nickname", `${member.nickname !== null ? `${member.nickname}`) : : 'None'}, true)
+    .addField(":calendar: Created At", `${moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY, HH;mm:ss")}`, true)
+    .addField(":robot: Bot Account", `${user.bot}`, true)
+    .addField("<:online:553477759863619604> Status", `${user.presence.status}`, true)
+    .addField(":joystick: Game", `${user.presence.game ? user.presence.game.name : 'None'}`)
+    .addField("Roles", member.roles.map(roles => `${roles.name}`).join(`, `), true)
+    message.channel.send({embed});
+
 }
 
 module.exports.help = {
