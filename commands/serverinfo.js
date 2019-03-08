247f45ae43d53d80fc43cf65fa22b257136aaa70
PritePasZ <bassky2512@gmail.com>
2019-03-08 14:32:46 +0700

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -15,7 +15,7 @@ module.exports.run = async (bot, message, args) => {
   .addField(":card_index: Server ID", message.guild.id, true)
   .addField(":clipboard: Join Date", message.member.joinedAt, true)
   .addField(":calendar: Server Create Date", message.guild.createdAt, true)
-  .setFooter("📋 Showing information about server");
+  .setFooter("📋 Shows the information about server");
 
   message.channel.send(serverembed);
 }
