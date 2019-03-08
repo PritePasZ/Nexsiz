4daa167351383de1faae625520bc864823b79c51
PritePasZ <bassky2512@gmail.com>
2019-03-08 13:53:52 +0700

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -7,14 +7,14 @@ module.exports.run = async (bot, message, args) => {
   .setColor("0ED4DA")
   .setThumbnail(message.guild.iconURL)
   .setAuthor(message.author.username, message.author.displayAvatarURL)
-  .addField(":bar_chart: Total Members", ${message.guild.memberCount})
-  .addField("<:bustinsilhouette:553086092837126145> Users", ${message.guild.memberCount - message.guild.members.filter(m => m.user.bot).size}, true)
-  .addField(":robot: Bots", ${message.guild.members.filter(m => m.user.bot).size}, true)
-  .addField("<:Owner:553078012283715636> Owner", ${message.guild.owner.user.tag}, true)
-  .addField(":earth_asia: Region", ${message.guild.region}, true)
-  .addField(":card_index: Server ID", ${message.guild.id}, true)
-  .addField(":clipboard: Join Date", ${message.member.joinedAt}, true)
-  .addField(":calendar: Server Create Date", ${message.guild.createdAt}, true);
+  .addField(":bar_chart: Total Members", message.guild.memberCount)
+  .addField("<:bustinsilhouette:553086092837126145> Users", message.guild.memberCount - message.guild.members.filter(m => m.user.bot).size, true)
+  .addField(":robot: Bots", message.guild.members.filter(m => m.user.bot).size, true)
+  .addField("<:Owner:553078012283715636> Owner", message.guild.owner.user.tag, true)
+  .addField(":earth_asia: Region", message.guild.region, true)
+  .addField(":card_index: Server ID", message.guild.id, true)
+  .addField(":clipboard: Join Date", message.member.joinedAt, true)
+  .addField(":calendar: Server Create Date", message.guild.createdAt, true);
 
   message.channel.send(serverembed);
 }
