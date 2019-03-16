dad56fedf4c754df99c000ed64667e7c5c45bef9
PritePasZ <bassky2512@gmail.com>
2019-03-16 16:19:43 +0700

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,8 @@
+module.exports.run = async (bot, msg, args) => {
+  let aTaged = msg.mentions.users.first();
+  msg.channel.send(`${aTaged.displayAvatarURL}`);
+}
+
+module.exports.help = {
+  name: "avatar"
+}
