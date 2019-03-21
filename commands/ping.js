7b97b6f6e7b4dec1f53719e7d35c3de52cd536b1
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-03-21 10:57:56 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,24 @@
-const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args, prefix) => {
+const Discord = require('discord.js');
 
-message.channel.send(`:ping_pong: Pong! \`${bot.pings[0]}ms\``);
+
+module.exports.run = async (bot, message, args) => {
+
+    const useruser = 'Command ran by: ' + message.author.username;
+    const userurl = message.author.avavtarURL;
     
-}
+    let embed = new Discord.RichEmbed() // Remember to use .MessageEmbed() if you use the master version
+        .setColor(0x43f033)
+        .setDescription(`Loading...`)
+        .setTimestamp()
+    message.channel.send(embed).then(message => {
+        embed.setColor(0x43f033)
+        embed.setDescription(`:ping_pong: Pong! **\`${client.pings[0]}ms\`**`)
+        embed.setFooter(useruser, userurl)
+        embed.setTimestamp()
+        message.channel.send(embed);
+
+} 
 module.exports.help = {
   name: "ping"
 }
