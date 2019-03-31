2e31f3530876c3dae39ed6c6d9b76d7e160f9ed1
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-03-31 09:59:42 +0700

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,28 +1,31 @@
-const Discord = require("discord.js");
+const discord = require('discord.js');
+const config = require('../config.json');
 
 module.exports.run = async (bot, message, args) => {
 
-  let rUser = message.guild.member(message.mention.users.first() || message.guild.members.get(args[0]));
-  if(!rUser) return mesage.channel.send("Couldn't find user.");
-  let reason = args.join(" ").slice(22);
-  
-  let reportEmbed = new Discord.RichEmbed()
-  .setDescription("Reports")
-  .setColor("#15f153")
-  .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
-  .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
-  .addField("Channel", message.channel)
-  .addField("Time", message.createdAt)
-  .addField("Reason", reason);
+    let target = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    let reason = args.slice(1).join(' ');
+    let reports = message.guild.channels.find('name', config.reportsChannel);
 
-  let reportschannel = message.guild.channel.find(`name`, "reports");
-  if(!reportschannel) return message.channel.send("Couldn't find report channel.")
+    if (!target) return message.reply('please specify a member to report!');
+    if (!reason) return message.reply('please specify a reason for this report!');
+    if (!reports) return message.reply(`please create a channel called ${config.reportsChannel} to log the reports!`);
 
-  message.delete().catch(O_o=>{});
-  reportschannel.send(reportEmbed);
+    let embed = new discord.RichEmbed()
+        .setColor('15f153')
+        .setThumbnail(target.user.avatarURL)
+        .addField('Reported Member', `${target.user.username} with an ID: ${target.user.id}`)
+        .addField('Reported By', `${message.author.username} with an ID: ${message.author.id}`)
+        .addField('Reported Time', message.createdAt)
+        .addField('Reported In', message.channel)
+        .addField('Reported Reason', reason)
+        .setFooter('Reported user imformation', target.user.displayAvatarURL);
 
-}
+    message.channel.send(`${target} was reported by ${message.author} for ${reason}`).then(msg => msg.delete(2000));
+    reports.send(embed);
+
+};
 
 module.exports.help = {
-  name: "report"
-}
+    name: 'report'
+};
