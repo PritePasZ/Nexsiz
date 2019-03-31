9cef9326a55aac64580ef2cf18395699f0bfc733
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-03-31 21:29:02 +0700

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -11,7 +11,7 @@ module.exports.run = async (bot, message, args) => {
     .setAuthor(message.member.displayName, message.author.displayAvatarURL)
     .setColor("#f49242")
     .setDescription(`:wastebasket: Cleared ${args[0]} messages.`)
-    .setFooter("Messages has been cleared.")
+    .setFooter("📋 Messages has been cleared.")
     .setTimestamp()
     message.channel.send(clearbotcommandsystem).then(msg => {msg.delete(2000)});
   });
