81182289dcfe9dd914896adbe27d2f9f255b2811
PritePasZ <bassky2512@gmail.com>
2019-04-01 15:13:29 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
   .setAuthor(message.member.displayName, message.author.displayAvatarURL)
   .setColor("#42f448")
   .setDescription(`:signal_strength: Bot Ping \n${Math.round(bot.ping)}ms`)
-  .setFooter("Made by PritePasZ","https://cdn.discordapp.com/avatars/346579836292300800/b02ca29aee1ec133c84377bd4235e957.png")
+  .setFooter("Made by PritePasZ#3071","https://cdn.discordapp.com/avatars/346579836292300800/b02ca29aee1ec133c84377bd4235e957.png")
    message.channel.send({embed});
 
 }
