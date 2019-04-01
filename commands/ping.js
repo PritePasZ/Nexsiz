0ec59d42ab01b6ccd28e24bfe89106b432a11aae
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-04-01 20:28:27 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (client, message, args) => {
 
   let embed = new Discord.RichEmbed()
   .setAuthor(message.author.username, message.author.displayAvatarURL)
   .setColor("#42f448")
-  .setDescription(`:signal_strength: Bot Ping \n${bot.ping}ms`)
+  .setDescription(`:signal_strength: API Latency \n${client.ping}ms`)
   .setFooter("Made by PritePasZ#3071 - Version 0.1.3b")
    message.channel.send({embed});
 
