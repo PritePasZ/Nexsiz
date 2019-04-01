cda8b22a955ad43234d2084b160bd85b5db72b25
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-04-01 20:24:38 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (client, message, args) => {
+module.exports.run = async (bot, message, args) => {
 
   let embed = new Discord.RichEmbed()
   .setAuthor(message.author.username, message.author.displayAvatarURL)
