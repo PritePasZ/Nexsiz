5cd3506fc38174a28950af9fd0283419e1fe290d
PritePasZ <bassky2512@gmail.com>
2019-04-01 14:42:50 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -6,7 +6,6 @@ module.exports.run = async (bot, message, args) => {
   .setAuthor(message.author.username, message.author.displayAvatarURL)
   .setColor("#42f448")
   .setDescription(`:signal_strength: Bot Ping : \`${Math.round(bot.ping)}ms\``)
-  .setFooter(":ping_pong: Pong!")
   .setTimeStamp()
    message.channel.send({embed});
 
