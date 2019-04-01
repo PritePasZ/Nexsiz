bef43324a23441f59ddab0fb1480eeb92cb3ab5d
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-04-01 13:09:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ bot.on("message", async message => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
 
-  let prefix = '*';
+  let prefix = 'n!';
   if (!message.content.startsWith(prefix)) return;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
