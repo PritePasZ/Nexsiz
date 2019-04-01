fe2a32b27cfb8d060563204b8f574138f1220a0d
PritePasZ <bassky2512@gmail.com>
2019-04-01 14:14:12 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-message.channel.send(`:computer: API Latency \`${Math.round(client.ping)}ms\``);
+message.channel.send(`:computer: API Latency \`${Math.round(bot.ping)}ms\``);
 
 }
 module.exports.help = {
