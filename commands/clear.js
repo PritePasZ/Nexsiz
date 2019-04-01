9feb71ae67e236c3edaf94790506f79bdd20500c
PritePasZ <bassky2512@gmail.com>
2019-04-01 16:19:37 +0700

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -4,12 +4,12 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("**:x: You dont have Manage Messages Permission!**");
-  if(!args[0]) return message.channel.send("**:x: Type the amount of value to clear the messages**!");
+  if(!args[0]) return message.channel.send("**:x: Type the amount of value to clear the messages!**");
   message.channel.bulkDelete(args[0]).then(() => {
 
     let clearbotcommandsystem = new Discord.RichEmbed()
     .setColor("#f49242")
-    .setDescription(`:wastebasket: Cleared ${args[0]} messages.`)
+    .setDescription(`**:wastebasket: Cleared** ${args[0]} **messages.**`)
     .setFooter("📋 Messages has been cleared.")
     .setTimestamp()
     message.channel.send(clearbotcommandsystem).then(msg => {msg.delete(5000)});
