577a0d9443e9b85d2091fd41a7192084ea6811a2
PritePasZ <bassky2512@gmail.com>
2019-04-01 14:45:34 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   let embed = new Discord.RichEmbed()
-  .setAuthor(message.author.username, message.author.displayAvatarURL)
+  .setAuthor(message.member.displayName, message.author.displayAvatarURL)
   .setColor("#42f448")
   .setDescription(`:signal_strength: Bot Ping : \`${Math.round(bot.ping)}ms\``)
    message.channel.send({embed});
