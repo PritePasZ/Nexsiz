56292aa4261418d9eeddb32911de69588f027104
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-04-01 19:48:49 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -6,6 +6,7 @@ module.exports.run = async (bot, message, args) => {
   .setAuthor(message.author.username, message.author.displayAvatarURL)
   .setColor("#42f448")
   .setDescription(`:signal_strength: Bot Ping \n${Math.round(bot.ping)}ms`)
+  .setDescription(`:computer: API Latency \n${Math.round(client.ping)}ms`)
   .setFooter("Made by PritePasZ#3071 - Version 0.1.3b")
    message.channel.send({embed});
 
