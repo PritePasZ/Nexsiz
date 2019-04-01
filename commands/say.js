6aea21cb947aac9755c585b162840cdb6dc3f8a0
PritePasZ <bassky2512@gmail.com>
2019-04-01 16:33:56 +0700

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-      if(!message.member.hasPermission("ADMINISTRATOR")) return;
+      if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("**:x: You dont have Administrator Permission!**");
       const sayMessage = args.join(" ");
       message.delete().catch();
       message.channel.send(sayMessage);
