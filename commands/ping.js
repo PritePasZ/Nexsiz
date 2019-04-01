4dab6435a316bb5052d91516123c2f48cb581deb
PritePasZ <bassky2512@gmail.com>
2019-04-01 15:14:34 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
   let embed = new Discord.RichEmbed()
   .setAuthor(message.member.displayName, message.author.displayAvatarURL)
   .setColor("#42f448")
-  .setDescription(`:signal_strength: Bot Ping \n ${Math.round(bot.ping)}ms`)
+  .setDescription(`:signal_strength: Bot Ping \n${Math.round(bot.ping)}ms`)
   .setFooter("Made by PritePasZ#3071","https://cdn.discordapp.com/avatars/346579836292300800/b02ca29aee1ec133c84377bd4235e957.png")
    message.channel.send({embed});
 
