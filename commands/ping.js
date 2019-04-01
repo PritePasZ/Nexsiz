150270dbf33888a4cca9eeb5b2aeb3dbed443c1a
PritePasZ <bassky2512@gmail.com>
2019-04-01 14:21:29 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-message.channel.send(`:signal_strength: Bot Ping : \`${Math.round(bot.ping)}ms\``);
+message.channel.send(`:signal_strength: Bot Ping : \`${Math.round(client.ping)}ms\``);
 
 }
 module.exports.help = {
