28a99775f82d478ad46a14e59b863d1875799895
PritePasZ <bassky2512@gmail.com>
2019-04-07 14:31:54 +0700

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("**:x: You dont have Administrator Permission!**").then(msg => {msg.delete(5000)})
   if (!args[0]) return message.channel.send(":x: Please type some question.");
   var question = args.join(" ");
-  let pollEmbed = new Discord.RichEmbed()\
+  let pollEmbed = new Discord.RichEmbed()
   .setAuthor(message.author.username, message.author.displayAvatarURL)
   .setColor("#f44141")
   .setDescription(args.join(' '))
