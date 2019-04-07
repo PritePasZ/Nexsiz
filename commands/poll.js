05d4b2505be4364172ea7ca3ce25b3e2d103315d
PritePasZ <bassky2512@gmail.com>
2019-04-07 14:25:36 +0700

    Create poll.js

---
+++ commands/poll.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message, args) => {
+
+  message.delete();
+  if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("**:x: You dont have Administrator Permission!**").then(msg => {msg.delete(5000)})
+  if (!args[0]) return message.channel.send(":no_entry: Please include a question.");
+  var question = args.join(" ");
+  let pollEmbed = new Discord.RichEmbed()
+  .setColor("#f44141")
+  .setDescription(args.join(' '))
+  .setTitle("Vote the message!");
+  message.channel.send(pollEmbed).then(function (message) {
+    message.react("✅")
+    message.react("❌")
+  });
+}
+
+module.exports.help = {
+  name: "poll"
+}
