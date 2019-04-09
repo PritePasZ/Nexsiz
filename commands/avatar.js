c74d3b95c251bdc2894d7b25762eb6147fdd0915
PritePasZ <bassky2512@gmail.com>
2019-04-09 16:48:19 +0700

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,13 +1,12 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (client, message, args) => {
   let aTaged = message.mentions.first() || message.member;
   let embed = new Discord.RichEmbed()
   .setColor("#000000")
   .setAuthor(message.author.username, message.author.displayAvatarURL)
   .setDescription(`<:bustinsilhouette:553086092837126145> Avatar user ${aTaged.user.username}`)
   .setImage(aTaged.user.displayAvatarURL)
-  .setFooter("Here is the avatar image")
   message.channel.send({embed});
 
 }
