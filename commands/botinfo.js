547d261a0a4bddbcf7ec720716cef5f4e0b122e7
PritePasZ <bassky2512@gmail.com>
2019-04-23 15:49:36 +0700

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -13,7 +13,7 @@ module.exports.run = async (bot, message, args) => {
     .setDescription("Made with <:nodejs:553212483415048221> Node.js and <:discordjs:553213251488317440> Discord.js\nHosted bot for free by <:heroku:553414334156374016> Heroku")
     .addField(`:clipboard: Created by`, `PritePasZ#3071`, true)
     .addField(`:information_source: Version`, `0.1.2b`, true)
-    .addField(`:tools: Debug`, `Updated to version 0.1.2b, new commands n!say and n!clear, n!poll (Moderator Commands)\nn!8ball (General Commands\nCheck out try n!help in the chat!`)
+    .addField(`:tools: Debug`, `Updated to version 0.1.2b, new commands\nCheck out try n!help in the chat!`)
     .addField(`:robot: Bot Created Date`, bot.user.createdAt)
     .setFooter("Made by PritePasZ","https://cdn.discordapp.com/avatars/346579836292300800/b02ca29aee1ec133c84377bd4235e957.png")
     .setTimestamp();
