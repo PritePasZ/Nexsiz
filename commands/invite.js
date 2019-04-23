4cde91cc1a82ef2db7da7d7739df86460f96b57b
PritePasZ <bassky2512@gmail.com>
2019-04-23 15:44:11 +0700

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -8,9 +8,9 @@ module.exports.run = async (bot, message, args) => {
   .setThumbnail(bicon)
   .setAuthor(bname, bicon)
   .setURL("https://discordapp.com/api/oauth2/authorize?client_id=552076002101297153&permissions=8&scope=bot")
-  .setTimestamp()
   .setTitle("Click here to invite Nexsiz in to your server!")
-  .setFooter("Made by PritePasZ","https://cdn.discordapp.com/avatars/346579836292300800/b02ca29aee1ec133c84377bd4235e957.png");
+  .setFooter("Made by PritePasZ","https://cdn.discordapp.com/avatars/346579836292300800/b02ca29aee1ec133c84377bd4235e957.png")
+  .setTimestamp();
 
   message.channel.send(inviteembed);
 }
