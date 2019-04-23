0218e990c74d3a8121c0a8cb8bc157a2c29b6ddb
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-04-23 19:18:50 +0700

    Update id.js

--- commands/id.js
+++ commands/id.js
@@ -3,9 +3,10 @@ const Discord = require("discord.js");
 module.exports.run =async (bot, message, args) => {
 
 const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
-let idembed = new Discord.RichEmbed()
+  let idembed = new Discord.RichEmbed(
+  .setColor(f44242)
+  .setAuthor(message.author.username, message.author.displayAvatarURL)
   .setDescription(`:card_index: User ID : \`${member.user.id}\`.`);
-  .setFooter("Searched by " + message.author.tag);
 
 }
 
