d6ef7e42c1428fc14a6d65e1af1d648c27821e2e
PritePasZ <bassky2512@gmail.com>
2019-04-23 18:18:57 +0700

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -4,11 +4,12 @@ module.exports.run = async (bot, message, args) => {
     let bicon = bot.user.displayAvatarURL;
     let supportembed = new Discord.RichEmbed()
     .setColor("#00ff00")
-    .setAuthor(message.author.username, message.author.displayAvatarURL))
+    .setAuthor(message.author.username, message.author.displayAvatarURL)
     .setThumbnail(bicon)
     .setTitle(":information_source: Support Info")
     .addField(":white_check_mark: **To see the bot commands use**", "!help")
     .addField("<:plus:557903316496154645> **Invite Nexsiz use**", "n!invite")
+    .setTimeStamp();
 
     message.channel.send(supportembed)
 
