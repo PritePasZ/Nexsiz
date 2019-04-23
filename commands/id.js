229257c644c32dceed08f05c9ceff74ff1f6951b
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-04-23 19:31:38 +0700

    Update id.js

--- commands/id.js
+++ commands/id.js
@@ -1,10 +1,12 @@
 const Discord = require("discord.js");
 
 module.exports.run =async (bot, message, args) => {
-
+        
 const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
 
-.message.channel.send:card_index: (`User ID : \`${member.user.id}\``);
+message.channel.send(`User ID: \`${member.user.id}\``);
+
+message.delete();
 
 }
 
