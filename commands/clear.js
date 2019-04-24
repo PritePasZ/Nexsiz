444964d23138acab64c69e82b4f8b00353641fc9
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-04-24 23:52:27 +0700

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
   
   if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("**:x: You dont have Manage Messages Permission!**").then(msg => {msg.delete(5000)});
   if(!args[0]) return message.channel.send("**:x: Type the amount of value to clear the messages!**").then(msg => {msg.delete(5000)});
-  if(!args[100]) return.messgaw.channel.send("**:x: Dont delete more than 100 messages!**").then(msg => {msg.delete(5000)});
+  if(!args[100]) return.message.channel.send("**:x: D
   message.channel.bulkDelete(args[0]).then(() => {
 
     let clearbotcommandsystem = new Discord.RichEmbed()
