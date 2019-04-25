ff486bc5682f5a7123ef3d85fc1bd6cb908f3a36
PritePasZ <bassky2512@gmail.com>
2019-04-25 16:56:54 +0700

    Update reverse.js

--- commands/reverse.js
+++ commands/reverse.js
@@ -1,7 +1,6 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-    if(!args[0]) return message.channel.send("**:x: Type the amount of value to reverse the messages!**").then(msg => {msg.delete(5000)});
+exports.run = function (bot, msg, args) {
+    if (args.length < 1) {
+        throw 'You must input text to be reversed!';
     }
     msg.reply(args.join(' ').split('').reverse().join(''));
 
@@ -11,3 +10,4 @@ module.exports.run = async (bot, message, args) => {
 module.exports.help = {
   name: "reverse"
 }
+© 2019 GitHub, Inc.
