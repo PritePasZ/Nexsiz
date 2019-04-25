f398a8b224d6eea3a41ada5b9a392c09c468c982
PritePasZ <bassky2512@gmail.com>
2019-04-25 16:55:43 +0700

    Update reverse.js

--- commands/reverse.js
+++ commands/reverse.js
@@ -1,4 +1,6 @@
-exports.run = function (bot, msg, args) {
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
     if(!args[0]) return message.channel.send("**:x: Type the amount of value to reverse the messages!**").then(msg => {msg.delete(5000)});
     }
     msg.reply(args.join(' ').split('').reverse().join(''));
