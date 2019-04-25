57fda0bd56c2427dbf21e4ba2c3c8c20fc0c12e8
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-04-25 23:16:02 +0700

    Update finduser.js

--- commands/finduser.js
+++ commands/finduser.js
@@ -2,7 +2,7 @@ module.exports.run = async (bot, message, args) => {
     let users = bot.users;
 
     let searchTerm = args[0];
-    if(!searchTerm) return message.channel.send(":x: Please type a term to search!**").then(msg => {msg.delete(5000)});
+    if(!searchTerm) return message.channel.send(":x: **Please type a term to search!**").then(msg => {msg.delete(5000)});
 
     let matches = users.filter(u => u.tag.toLowerCase().includes(searchTerm.toLowerCase()));
     
