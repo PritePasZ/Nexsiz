104e62896d5fef541e606a3fba77e004b6b08300
PritePasZ <bassky2512@gmail.com>
2019-04-25 17:32:50 +0700

    Create clap.js

---
+++ commands/clap.js
@@ -0,0 +1,12 @@
+const randomizeCase = word => word.split('').map(c => Math.random() > 0.5 ? c.toUpperCase() : c.toLowerCase()).join('');
+
+exports.run = (bot, message, args) => {
+    if (args.length < 1) return message.channel.send("Please provide some text to clapify")
+
+    message.channel.send(args.map(randomizeCase).join(':clap:'));
+
+}
+
+module.exports.help = {
+    name: "clap"
+}
