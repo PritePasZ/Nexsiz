0e37e2ac72f5bb77299e5710d9d1186a9cdada20
PritePasZ <bassky2512@gmail.com>
2019-04-25 17:38:55 +0700

    Update clap.js

--- commands/clap.js
+++ commands/clap.js
@@ -1,7 +1,7 @@
 const randomizeCase = word => word.split('').map(c => Math.random() > 0.5 ? c.toUpperCase() : c.toLowerCase()).join('');
 
 exports.run = (bot, message, args) => {
-    if (args.length < 1) return message.channel.send("**:information_source: Please provide some text to clapify**")
+    if (args.length < 1) return message.channel.send("**:x: Please provide some text to clapify**").then(msg => {msg.delete(5000)});
 
     message.channel.send(args.map(randomizeCase).join(':clap:'));
 
