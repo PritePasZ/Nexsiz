6109b5a12f951ebbae42a5bbde47e1318ea81066
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-04-25 23:55:17 +0700

    Update mock.js

--- commands/mock.js
+++ commands/mock.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const randomizeCase = word => word.split('').map(c => Math.random() > 0.5 ? c.toUpperCase() : c.toLowerCase()).join('');
 
 exports.run = (bot, message, args) => {
-    if (args.length < 1) return message.channel.send("Please provide some text to Mock")
+    if (args.length < 1) return message.channel.send(":x: **Please provide some text to Mock**")
 
     let mockEmbed = new Discord.RichEmbed()
     .setColor("f4f442")
