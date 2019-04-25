2a919bac98b32ae46fd41ffa76ae363f013feba0
PritePasZ <bassky2512@gmail.com>
2019-04-25 16:59:01 +0700

    Update reverse.js

--- commands/reverse.js
+++ commands/reverse.js
@@ -1,6 +1,6 @@
 exports.run = function (bot, msg, args) {
     if (args.length < 1) {
-        throw 'You must input text to be reversed!'.then(msg => {msg.delete(5000)});
+        throw ("You must input text to be reversed!").then(msg => {msg.delete(5000)});
     }
     msg.reply(args.join(' ').split('').reverse().join(''));
 
