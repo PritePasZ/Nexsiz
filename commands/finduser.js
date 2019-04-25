71b512cf90fefc7b17e2273c9cf70c582b936e20
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-04-25 23:04:04 +0700

    Create finduser.js

---
+++ commands/finduser.js
@@ -0,0 +1,17 @@
+module.exports.run = async (bot, message, args) => {
+    let users = bot.users;
+
+    let searchTerm = args[0];
+    if(!searchTerm) return message.channel.send(":x: Please type a term to search!**").then(msg => {msg.delete(5000)});
+
+    let matches = users.filter(u => u.tag.toLowerCase().includes(searchTerm.toLowerCase()));
+    
+    message.channel.send(matches.map(u => u.tag));
+
+    message.delete();
+
+     }
+    
+        module.exports.help = {
+            name: "finduser"
+        }
