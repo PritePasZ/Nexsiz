5e98f818ec951370c232f16ee6b0d36a8b7ae089
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-04-25 23:11:43 +0700

    Update finduser.js

--- commands/finduser.js
+++ commands/finduser.js
@@ -6,9 +6,13 @@ module.exports.run = async (bot, message, args) => {
 
     let matches = users.filter(u => u.tag.toLowerCase().includes(searchTerm.toLowerCase()));
     
-    message.channel.send(matches.map(u => u.tag));
+    let finduserembed = new Discord.RichEmbed()
+    .setAuthor(message.author.username, message.author.displayAvatarURL)
+    .setColor("#f44242")
+    .setTimeStamp()
+    .setDescription(matches.map(u => u.tag));
 
-    message.delete();
+
 
      }
     
