1aa8833ae0162ffacf8dce37b3eec69e6d6287b5
PritePasZ <bassky2512@gmail.com>
2019-04-28 18:12:13 +0700

    Update feedback.js

--- commands/feedback.js
+++ commands/feedback.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
   if(args[0] == "help"){
     let helpembxd = new Discord.RichEmbed()
     .setColor("#f44242")
-    .addField("Feedback Command", "**Usage: n!eedback <reason>**")
+    .addField("Feedback Command", "**Usage: n!feedback <reason>**")
 
     message.channel.send(helpembxd);
     return;
