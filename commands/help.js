3dbc931a5cf97d6be8ebb6304439406408e872c2
PritePasZ <bassky2512@gmail.com>
2019-04-30 16:46:45 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, prefix) => {
 
+  const pidor = message.guild.members.get(args[0]) || message.member;
+
 let dm = new Discord.RichEmbed()
     .setColor(`#409cd9`)
     .setAuthor(message.author.username, message.author.displayAvatarURL)
@@ -10,15 +12,43 @@ let dm = new Discord.RichEmbed()
     message.channel.send(dm);
 
 let helpembed = new Discord.RichEmbed()
-    .setColor(`#4286f4`)
+    .setColor(`00ff00`)
     .setAuthor(`Commands & Guides`, bot.user.avatarURL)
+    .addField(`:information_source: Test`, `**n!help`)
+
+    pidor.send(helpembed);
+
+let generalembed = new Discord.RichEmbed()
+    .setColor(`#4286f4`)
     .addField(`:desktop: General Commands`, `**n!help** - Show a list of commands & guides to your DM.\n**n!feedback** - reports bug and contact to <@346579836292300800>.\n**n!invite** - to invite <@552076002101297153> in to your server.\n**n!icon** - gives you the server icon\n**n!id** - show ID of user\n**n!8ball** - random the answer from the question. \n**n!avatar** - show the avatar user.\n**n!ping** - Show the current user ping/ms.\n**n!support - using for help**\n**n!serverinfo** - Shows the information of the guild.\n**n!botinfo** - Shows the information of the bot.\n**n!userinfo** Shows the information of your.\n**n!unicode** - change capital to number`)
+
+    pidor.send(generalembed);
+
+let moderatembed = new Discord.RichEmbed()
+    .setColor(`#ff3a3a`)
     .addField(`:tools: Moderation Commands`, `**n!clear** [amount] - Clear the given amount of messages.\n**n!say** [text] - <@552076002101297153> will chat with your given text.\n**n!poll** - vote the message from user are typed.`)
+
+    pidor.send(moderatembed);
+
+let funembed = new Discord.RichEmbed()
+    .setColor(`#f4b942`)
     .addField(`<:epic:452254851003318282> Fun Commands`, `**n!reverse** - type to amount to reverse the message\n**n!lenny** - will message ( ͡° ͜ʖ ͡°)\n**n!pepe** - random pepe image.\n**n!kill** - or mention to kill him/her or suicide.\n**n!shrug** - ¯\\_\(ツ)\_\/¯ \n**n!clap** - clapify at the space of message.\n**n!megusta** - <@552076002101297153> will message megusta image.\n**n!morse** - translate from text to morse code/translate from morse code to text.\n**n!mock** - put the text to mock.\n**n!flip** - to the message.`)
+
+    pidor.send(funembed);
+
+let musicembed = new Discord.RichEmbed()
+    .setColor(`#41c1f4`)
     .addField(`<:headphone:553459778714730527> Music Commands`, `Music Commands is coming soon\nwaiting for PritePasZ will add the commands`)
     .setFooter("Made By PritePasZ", "https://cdn.discordapp.com/avatars/346579836292300800/b02ca29aee1ec133c84377bd4235e957.png")
     .setTimestamp()
-    message.author.send(helpembed);
+
+let chanEmbed = new Discord.RichEmbed()
+    .setTitle("Help")
+    .setColor("#0ff00")
+    .setFooter(`Help command used by: ${pidor.user.username}`)
+    .setDescription(`${pidor} Check your DMs`);
+
+    message.channel.send(chanEmbed)
 
 }
 module.exports.help = {
