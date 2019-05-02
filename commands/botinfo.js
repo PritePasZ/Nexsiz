8ccd976d9d9c78aea1077b0c4279d6e6e617e87a
PritePasZ <bassky2512@gmail.com>
2019-05-02 16:49:52 +0700

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let version = `0.1.7a`;
+    let version = `0.1.8a`;
     let bicon = bot.user.displayAvatarURL;
     let bname = bot.user.username;
     let botembed = new Discord.RichEmbed()
