939d28023efec5afbe9da291b998bb460fb97658
PritePasZ <bassky2512@gmail.com>
2019-05-02 16:12:21 +0700

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -6,12 +6,12 @@ module.exports.run = async (bot, message, args) => {
   let {body} = await superagent
   .get(`http://edgecats.net/random`);
 
-  let dogembed = new Discord.RichEmbed()
+  let catembed = new Discord.RichEmbed()
   .setColor("#ffa202")
   .setDescription(":cat: Here's the random cat! <a:glitchcat:537598075502460941>")
   .setImage(body.message);
 
-  message.channel.send(dogembed);
+  message.channel.send(catembed);
 
 }
 
