1105d099883eae9bee8c83720bf155698aa28454
PritePasZ <bassky2512@gmail.com>
2019-05-02 16:49:21 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -15,7 +15,7 @@ let helpembed = new Discord.RichEmbed()
 
 let generalembed = new Discord.RichEmbed()
     .setColor(`#4286f4`)
-    .addField(`:desktop: General Commands`, `**n!icon** - Gives you the server icon\n**n!id** - Show ID of user\n**n!8ball** - Random the answer from the question. \n**n!avatar** - Show the avatar user.\n**n!ping** - Show the current user ping/ms.\n**n!support** - Using for help\n**n!serverinfo** - Shows the information of the guild.\n**n!botinfo** - Shows the information of the bot.\nguilds - Shows a list of <@552076002101297153> guilds.\n**n!userinfo** Shows the information of your.\n**n!unicode** - change capital to number\n**n!weather** - Show the current weather statistics.`)
+    .addField(`:desktop: General Commands`, `**n!icon** - Gives you the server icon\n**n!id** - Show ID of user\n**n!8ball** - Random the answer from the question. \n**n!avatar** - Show the avatar user.\n**n!ping** - Show the current user ping/ms.\n**n!support** - Using for help\n**n!serverinfo** - Shows the information of the guild.\n**n!botinfo** - Shows the information of the bot.\nguilds - Shows a list of <@552076002101297153> guilds.\n**n!userinfo** Shows the information of your.\n**n!unicode** - change capital to number`)
 
     pidor.send(generalembed);
 
