8ad842f42afeaf4cb0f336d040a786745ae38c68
PritePasZ <bassky2512@gmail.com>
2019-05-02 16:08:21 +0700

    Create cat.js

---
+++ commands/cat.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const superagent = require("superagent");
+
+module.exports.run = async (bot, message, args) => {
+
+  let {body} = await superagent
+  .get(`http://edgecats.net/random`);
+
+  let dogembed = new Discord.RichEmbed()
+  .setColor("#ffa202")
+  .setDescription(":cat: Here's the random cat! <a:glitchcat:537598075502460941>")
+  .setImage(body.message);
+
+  message.channel.send(dogembed);
+
+}
+
+module.exports.help = {
+  name: "dog"
+}
