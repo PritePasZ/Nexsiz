3bdf1475425978c02d72055d713a971a7c5eefb5
PritePasZ <bassky2512@gmail.com>
2019-05-03 15:05:30 +0700

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
   let inviteembed = new Discord.RichEmbed()
   .setColor("#4286f4")
   .setAuthor(bname, bicon)
-  .setURL("https://discordapp.com/api/oauth2/authorize?client_id=552076002101297153&permissions=8&scope=bot")
+  .setURL("https://discordapp.com/oauth2/authorize?client_id=552076002101297153&scope=bot&permissions=2146954495")
   .setDescription("<:OOFthumbsup:452254913427275777> **Want to invite Nexsiz?, Just click at up there!**")
   .setTitle(":arrow_right: Click here to invite Nexsiz in to your server! <:plus_1:557908827840970773>")
   .setFooter("Made by PritePasZ","https://cdn.discordapp.com/avatars/346579836292300800/b02ca29aee1ec133c84377bd4235e957.png")
