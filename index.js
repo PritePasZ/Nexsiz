ff8fddb3a22709c67f7e9fc0f8f28ce73965983e
PritePasZ <bassky2512@gmail.com>
2019-05-03 15:32:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const fs = require("fs");
-const botconfig = require("../botconfig.json");
 const client = new Discord.Client();
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
