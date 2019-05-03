16378cb1fd886b33fd7e79d06567e80f81d2b749
PritePasZ <bassky2512@gmail.com>
2019-05-03 17:26:20 +0700

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,20 +1,19 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let msg = await message.channel.send("<a:loading:573815730504597505> Generating avatar...");
+let msg = await message.channel.send("<a:loading:573815730504597505> Generating avatar...");
 
-    let mentionedUser = message.mentions.users.first() || message.author;
+let mentionedUser = message.mentions.users.first() || message.author;
 
-        let embed = new Discord.RichEmbed()
+let avatarembed = new Discord.RichEmbed()
+.setImage(mentionedUser.displayAvatarURL)
+.setColor("#f4d442")
+.setTitle("<:bustinsilhouette:553086092837126145> Here is the Avatar")
+.setFooter("Searched by " + message.author.tag)
+.setTimestamp()
+.setDescription("[Avatar URL link]("+mentionedUser.displayAvatarURL+")");
 
-        .setImage(mentionedUser.displayAvatarURL)
-        .setColor("#f4d442")
-        .setTitle("<:bustinsilhouette:553086092837126145> Here is the Avatar")
-        .setFooter("Searched by " + message.author.tag)
-        .setTimestamp()
-        .setDescription("[Avatar URL link]("+mentionedUser.displayAvatarURL+")");
-
-        message.channel.send(embed)
+    message.channel.send(avatarembed)
 
 
 
