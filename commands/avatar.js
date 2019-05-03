a27c7afb47da3e29d4bd0dcd412957ef42985b54
PritePasZ <bassky2512@gmail.com>
2019-05-03 17:23:08 +0700

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let msg = await message.channel.send("<a:loading:573815730504597505> Generating avatar...")
+    let msg = await message.channel.send("<a:loading:573815730504597505> Generating avatar...");
 
     let mentionedUser = message.mentions.users.first() || message.author;
 
