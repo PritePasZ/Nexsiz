bb13f50b476c7f8f83db4b34349ed26dbc612220
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-05-05 13:24:00 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -6,10 +6,11 @@ module.exports.run = async (client, message, args) => {
   .setAuthor(message.author.username, message.author.displayAvatarURL)
   .setColor("#42f448")
   .setDescription(`:computer: **API Latency** - ${client.ping}ms`)
-  .setFooter("Made by PritePasZ#3071 - Version 0.1.3b")
+  .setFooter("Made by PritePasZ#3071")
    message.channel.send({embed});
 
 }
+
 module.exports.help = {
   name: "ping"
 }
