af6af5d06aeee8acec9085755e45e501d74a8e88
PritePasZ <bassky2512@gmail.com>
2019-05-05 16:19:01 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args) => {
   let embed = new Discord.RichEmbed()
   .setAuthor(message.author.username, message.author.displayAvatarURL)
   .setColor("#42f448")
-  .setDescription(`:computer: **API Latency** - ${client.ping[0]}ms`)
+  .setDescription(`:computer: **API Latency** - ${client.pings[0]}ms`)
   .setFooter("Made by PritePasZ#3071")
    message.channel.send({embed});
 
