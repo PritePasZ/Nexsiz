87bcb2013f1d7cdd055305c703b83d32463f0a6d
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-05-07 16:31:36 +0700

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -10,7 +10,7 @@ const Discord = require("discord.js");
         return `${days.padStart(1, '0')} days, ${hrs.padStart(2, '0')} hours, ${min.padStart(2, '0')} minutes, ${sec.padStart(2, '0')} seconds, `
     }
 
-       message.channel.send("`**I have been online for :** ${duration(bot.uptime)}`);
+       message.channel.send(`**I have been online for :** ${duration(bot.uptime)}`);
 
  }
 
