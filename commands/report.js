1f4f3c9da9d3363b71c6f1e9232ea6ebbbc07959
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-05-09 21:47:27 +0700

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!rUser) return message.channel.send("Couldn't find user.");
+    if(!rUser) return message.channel.send(":x: **Couldn't find user.**");
     let rreason = args.join(" ").slice(22);
 
     let reportEmbed = new Discord.RichEmbed()
@@ -15,7 +15,7 @@ module.exports.run = async (bot, message, args) => {
     .addField(":pencil: Reason", rreason);
 
     let reportschannel = message.guild.channels.find(`name`, "reports");
-    if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
+    if(!reportschannel) return message.channel.send(":x: **Couldn't find reports channel.**");
 
 
     message.delete().catch(O_o=>{});
