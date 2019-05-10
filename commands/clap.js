e59b1f6197ea37f2adf971adfc2f1b729ed06b3c
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-05-10 20:40:12 +0700

    Update clap.js

--- commands/clap.js
+++ commands/clap.js
@@ -1,7 +1,7 @@
 const randomizeCase = word => word.split('').map(c => Math.random() > 0.5 ? c.toUpperCase() : c.toLowerCase()).join('');
 
 exports.run = (bot, message, args) => {
-    if (args.length < 1) return message.channel.send("**:x: Please provide some text to clapify**").then(msg => {msg.delete(5000)});
+    if (args.length < 1) return message.channel.send("**<:tickNo:315009174163685377> Please provide some text to clapify**").then(msg => {msg.delete(5000)});
     if(!args[1]) return message.reply(":x: **Please type more than 1 word!**");
     
     message.channel.send(args.map(randomizeCase).join(':clap:'));
