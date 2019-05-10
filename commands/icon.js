1a242ad5379a380cc8c57dbf7f75456e287676e1
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-05-10 20:45:26 +0700

    Update icon.js

--- commands/icon.js
+++ commands/icon.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 let msg = await message.channel.send("<a:loading:573815730504597505> Generating icon...");
 
-if(!message.guild.iconURL) return msg.edit(":x: **The server not have an icon!**");
+if(!message.guild.iconURL) return msg.edit("<:tickNo:315009174163685377> **The server not have an icon!**");
 
 let iconembed = new Discord.RichEmbed()
 .setColor("f4d442")
