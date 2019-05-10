376c49cac50728881262b8f47f5176367a0fdcf6
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-05-10 20:52:21 +0700

    Update reverse.js

--- commands/reverse.js
+++ commands/reverse.js
@@ -1,4 +1,6 @@
-exports.run = function (bot, msg, args) {
+const Discord = require("discord.js");
+
+module.exports.run = function (bot, msg, args) {
     if (args.length < 1) return message.channel.send("<:tickNo:315009174163685377> Please inpit the text to be reversed!").then(msg => {msg.delete(5000)});
 
     }
