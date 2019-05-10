ea00ef35f9a222a0e662931e6ec4b5f4bc788552
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-05-10 20:50:47 +0700

    Update reverse.js

--- commands/reverse.js
+++ commands/reverse.js
@@ -1,6 +1,6 @@
 exports.run = function (bot, msg, args) {
-    if (args.length < 1) {
-        throw 'You must input text to be reversed!';
+    if (args.length < 1) return message.channel.send("<:tickNo:315009174163685377> Please inpit the text to be reversed!").then(msg => {msg.delete(5000)});
+
     }
     msg.reply(args.join(' ').split('').reverse().join(''));
 
