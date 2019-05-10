1d719f11c78534abed13ea06ef520ae678ff4b5c
PritePasZ <bassky2512@gmail.com>
2019-05-10 17:13:28 +0700

    Create report.js

---
+++ commands/report.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply(":x: **You dont have Manage Messages Permisson!**");
+    if(!rUser) return message.channel.send(":x: **Couldn't find user.**");
+    let rreason = args.join(" ").slice(22);
+
+    let reportEmbed = new Discord.RichEmbed()
+    .setDescription(":pushpin: Reports")
+    .setColor("#15f153")
+    .addField(":inbox_tray: Reported User", `${rUser} with ID: ${rUser.id}`)
+    .addField(":incoming_envelope:  Reported By", `${message.author} with ID: ${message.author.id}`)
+    .addField(":clipboard: Channel", message.channel)
+    .addField(":pencil: Reason", rreason);
+
+    reportschannel.send(reportEmbed).then(msg => {msg.delete(5000)});
+
+}
+
+module.exports.help = {
+  name: "report"
+}
