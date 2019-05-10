13015544019eddc6d82f281cb02391413109da81
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-05-10 21:05:55 +0700

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
       if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("**<:tickNo:315009174163685377> You dont have **Manage Messages** Permission!**").then(msg => {msg.delete(5000)});
+      if(!args[0]) return message.channel.send("**<:tickNo:315009174163685377> Please, Type some message**").then(msg => {msg.delete(5000)});
       const sayMessage = args.join(" ");
       message.delete().catch();
       message.channel.send(sayMessage);
