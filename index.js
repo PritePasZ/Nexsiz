880dcc1b5e212e184a9da736dfe9d4d0621fbc86
PritePasZ <bassky2512@gmail.com>
2019-06-01 14:10:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,6 @@ bot.on('guildMemberRemove', member => {
   .setColor(`#ff3320`)
   channel.send(goodbyeembed);
  });
-
 if(!message.content.startsWith(prefix)) return;
   // Cooldown feature.
   if(cooldown.has(message.author.id)){
