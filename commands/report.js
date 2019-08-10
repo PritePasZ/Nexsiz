359606553cf96c45f5e689b4809fa7d8496c2962
PritePasZ <bassky2512@gmail.com>
2019-08-10 16:05:22 +0700

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("<:tickNo:315009174163685377> **You dont have Manage Messages Permisson!**");
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("<:tickNo:576414524014329857> **You dont have Manage Messages Permisson!**");
     if(!rUser) return message.channel.send("<:tickNo:576414524014329857> **Couldn't find user.**");
     let rreason = args.join(" ").slice(22);
 
