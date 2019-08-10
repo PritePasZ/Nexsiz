b13f3559935736420a9a965b922f0abecc82bf8d
PritePasZ <bassky2512@gmail.com>
2019-08-10 16:11:17 +0700

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -3,13 +3,13 @@ const Discord = require('discord.js')
 module.exports.run = async (bot, message, args) => {
 
   message.delete();
-  if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("**:x: You dont have Administrator Permission!**").then(msg => {msg.delete(5000)})
-  if (!args[0]) return message.channel.send(":x: Please type some question.");
+  if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("**<:tickNo:576414524014329857> You dont have Administrator Permission!**").then(msg => {msg.delete(5000)})
+  if (!args[0]) return message.channel.send("<:tickNo:576414524014329857> Please type some question.");
   var question = args.join(" ");
   let pollEmbed = new Discord.RichEmbed()
   .setAuthor(message.author.username, message.author.displayAvatarURL)
   .setColor("#f44141")
-  .setTitle("<a:party10000:525854556815360001> Vote the message! <a:hyperpin:537598127407104020>")
+  .setTitle(":tada: Vote the message! <:GWpingSock:400765764682121226>")
   .setDescription(args.join(' '))
   .setFooter("React by click the emoji - Made by PritePasZ#3071")
   message.channel.send(pollEmbed).then(function (message) {
