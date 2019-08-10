605b39fa2743839af0ccc3e4281c2e2b1e57f3dc
PritePasZ <bassky2512@gmail.com>
2019-08-10 15:56:44 +0700

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -9,7 +9,7 @@ module.exports.run = async (bot, message, args) => {
   .setURL("https://discordapp.com/oauth2/authorize?client_id=552076002101297153&scope=bot&permissions=2146954495")
   .setDescription(":thumbsup::skin-tone-1: **Want to invite Nexsiz?, Just click at up there!**")
   .setTitle(":arrow_right: Click here to invite Nexsiz in to your server! <:plus_1:557908827840970773>")
-  .setFooter("Made by PritePasZ","https://cdn.discordapp.com/avatars/346579836292300800/b02ca29aee1ec133c84377bd4235e957.png")
+  .setFooter("Made by PritePasZ","https://cdn.discordapp.com/avatars/346579836292300800/63ca26aec73a01c20bd6f0f97667186f.png")
   .setTimestamp();
 
   message.channel.send(inviteembed);
