c72f6ac51417a14a311d581016c84c733feaccd0
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-10-17 10:18:22 +0700

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let version = `0.1.9a`;
+    let version = `0.2.0a`;
     let bicon = bot.user.displayAvatarURL;
     let bname = bot.user.username;
     let botembed = new Discord.RichEmbed()
@@ -10,13 +10,13 @@ module.exports.run = async (bot, message, args) => {
     .setAuthor(bname, bicon)
     .setURL(`https://discord.gg/2Zvgkcy`)
     .setTimestamp()
-    .setTitle(`Click here to join PritePasZ discord server`)
+    .setTitle(`Click here to join CorosiveZ discord server`)
     .setDescription("Made with <:nodejs:553212483415048221> Node.js and <:discordjs:553213251488317440> Discord.js\nHosted bot for free by <:heroku:553414334156374016> Heroku")
-    .addField(`:clipboard: Created by`, `PritePasZ#3071`, true)
+    .addField(`:clipboard: Created by`, `CorosiveZ#3071`, true)
     .addField(`:information_source: Version`, `${version}`, true)
-    .addField(`:tools: Debug`, `Updated to version ${version}, new commands - Fun Commands!\n To invite Nexsiz in to your server! type "n!invite"\nCheck out try n!help in the chat!`)
+    .addField(`:tools: Debug`, `Updated to version ${version}, new commands - Kick & Ban commands in moderation category\n To invite Nexsiz in to your server! type "n!invite"\nCheck out try n!help in the chat!`)
     .addField(`:robot: Bot Created Date`, bot.user.createdAt)
-    .setFooter("Made by PritePasZ","https://cdn.discordapp.com/avatars/346579836292300800/b02ca29aee1ec133c84377bd4235e957.png")
+    .setFooter("Made by Corosive","https://cdn.discordapp.com/avatars/346579836292300800/b02ca29aee1ec133c84377bd4235e957.png")
     .setTimestamp();
 
     message.channel.send(botembed);
