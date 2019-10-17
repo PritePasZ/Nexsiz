3b9a93c6bba06bb7375cb8fa8efb82d9a4f7a03a
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-10-17 10:13:42 +0700

    Create ban.js

---
+++ commands/ban.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!bUser) return message.channel.send("Can't find user!");
+    let bReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("<:tickNo:576414524014329857> You can't banned the person who has administrator permission!");
+    if(bUser.hasPermission("ADMINISTRATOR")) return message.channel.send("<:tickNo:576414524014329857> That person can't be banned!");
+
+    let banEmbed = new Discord.RichEmbed()
+    .setDescription(":pencil: Ban Log :pencil:")
+    .setColor("#bc0000")
+    .addField(":speaking_head: Banned User:speaking_head:", `${bUser} with ID ${bUser.id}`)
+    .addField(":mag: Banned By :mag:", `<@${message.author.id}> with ID ${message.author.id}`)
+    .addField(":stopwatch: Time :stopwatch:", message.createdAt)
+    .addField(":inbox_tray: Reason :inbox_tray:", bReason);
+
+    let incidentchannel = message.guild.channels.find(`name`, "incidents :bar_chart:");
+    if(!incidentchannel) return message.channel.send("<:tickNo:576414524014329857> Can't find incidents channel.");
+
+    message.guild.member(bUser).ban(bReason);
+    incidentchannel.send(banEmbed);
+}
+
+module.exports.help = {
+  name:"ban"
+}
