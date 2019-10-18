d877c9dfc94c00005ecfbd367b100ebb9d3cffef
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-10-18 09:01:18 +0700

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("**<:tickNo:576414524014329857> You dont have Manage Messages Permission!**").then(msg => {msg.delete(5000)});
-  if(args >= 100) return message.channel.send("<:tickNo:576414524014329857> You cannot clear more than 100 messages at one time!");
+  if(args >= 100) return message.channel.send("<:tickNo:576414524014329857> You cannot clear more than 100 messages at one time!").then(msg => {msg.delete(5000)});
   if(!args[0]) return message.channel.send("**<:tickNo:576414524014329857> Type the amount of value to clear the messages!**").then(msg => {msg.delete(5000)});
   message.channel.bulkDelete(args[0]).then(() => {
 
