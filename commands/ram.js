679790739039af7593d775a96a59ce7403dcdc81
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-10-18 10:02:46 +0700

    Update ram.js

--- commands/ram.js
+++ commands/ram.js
@@ -4,6 +4,7 @@ module.exports.run = async (bot, message, args) => {
 
   let ramEmbed = new Discord.RichEmbed()
   .setColor("#fc03c6")
+  .setDescription(`<:smugram:634586554223820820> Here's Ram!`)
   .setImage(`https://cdn.discordapp.com/attachments/552376732964093963/634585194153639949/DYToB2e_d.jpg`);
 
   message.channel.send(ramEmbed);
