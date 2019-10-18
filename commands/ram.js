f1bb99791e0397c128faf1a84d271f52acb2364d
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2019-10-18 09:45:56 +0700

    Update ram.js

--- commands/ram.js
+++ commands/ram.js
@@ -5,11 +5,11 @@ module.exports.run = async (bot, message, args) => {
   let {body} = await superagent
   .get(`https://i.imgur.com/DYToB2e_d.jpg?maxwidth=640&shape=thumb&fidelity=medium`);
 
-  let ramembed = new Discord.RichEmbed()
+  let ramEmbed = new Discord.RichEmbed()
   .setColor("#fc03c6")
   .setImage(body.file);
 
-  message.channel.send(ramembed);
+  message.channel.send(ramEmbed);
 
 }
 
