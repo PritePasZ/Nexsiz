d392de1747e0279c58ed079bf9f1aeef25b02d97
PritePasZ <bassky2512@gmail.com>
2019-10-19 14:10:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ fs.readdir("./commands/", (err, files) => {
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online, Bot is ready and working in ${bot.guilds.size} servers with ${bot.users.size} users!`);
-  bot.user.setActivity("with CorosiveZ | n!help", {type: "WATCHING"});
+  let status = statues(Math.floor(Math.random() * statues.length)];
+  bot.user.setActivity(`${bot.guilds.size} servers | n!help`, {type: "WATCHING"});
 });
 
 
