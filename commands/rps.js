7bb3cfe0fd47b437334ebaff1f504824a231b089
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2020-05-07 09:05:59 +0700

    Update rps.js

--- commands/rps.js
+++ commands/rps.js
@@ -2,32 +2,32 @@ const Discord = require("discord.js");
 
 module.exports.run = (bot, message, args) => {
 
-    let rock2 = ["Paper! I win!", "Scissors! You win!"]
+    let rock2 = [":tada: Paper! I win!", "Scissors! You win!"]
     let rock1 = Math.floor(Math.random() * rock2.length);
 
-    let paper2 = ["Rock! You win!", "Scissors! I win!"]
+    let paper2 = [":tada: Rock! You win!", "Scissors! I win!"]
     let paper1 = Math.floor(Math.random() * paper2.length);
 
-    let scissors2 = ["Rock! I win!", "Paper! You win!"]
+    let scissors2 = [":tada: Rock! I win!", "Paper! You win!"]
     let scissors1 = Math.floor(Math.random() * scissors2.length);
 
   let rock = new Discord.RichEmbed()
   .setAuthor("Rock, Paper, Scissors")
-  .setColor("#6b5858")
+  .setColor("#00BDFF")
   .addField("You Chose", `${args[0]}`)
-  .addField("I choose", rock2[rock1])
+  .addField("I Choose", rock2[rock1])
 
   let paper = new Discord.RichEmbed()
   .setAuthor("Rock, Paper, Scissors")
-  .setColor("#6b5858")
+  .setColor("#00BDFF")
   .addField("You Chose", `${args[0]}`)
-  .addField("I choose", paper2[paper1])
+  .addField("I Choose", paper2[paper1])
 
   let scissors = new Discord.RichEmbed()
   .setAuthor("Rock, Paper, Scissors")
-  .setColor("#6b5858")
+  .setColor("#00BDFF")
   .addField("You Chose", `${args[0]}`)
-  .addField("I choose", scissors2[scissors1])
+  .addField("I Choose", scissors2[scissors1])
 
 
   if (message.content === "n!rps rock") message.channel.send(rock)
