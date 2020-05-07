b97a4d06067307de87b84d416c0b6d8e56cd20db
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2020-05-07 08:48:43 +0700

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -10,13 +10,12 @@ module.exports.run = async (bot, message, args) => {
     .setAuthor(bname, bicon)
     .setURL(`https://discord.gg/2Zvgkcy`)
     .setTimestamp()
-    .setTitle(`Click here to join HaiCiaZ Discord Server`)
+    .setTitle(`Click here to join our discord server`)
     .setDescription("Made with <:nodejs:553212483415048221> Node.js and <:discordjs:553213251488317440> Discord.js\nHosted bot for free by <:heroku:553414334156374016> Heroku")
-    .addField(`:clipboard: Created by`, `PritePasZ#3071`, true)
+    .addField(`:clipboard: Created by`, `PriteToKung#3071`, true)
     .addField(`:information_source: Version`, `${version}`, true)
     .addField(`:tools: Debug`, `Updated to version ${version}, new commands - Kick & Ban commands in moderation category\n To invite Nexsiz in to your server! type "n!invite"\nCheck out try n!help in the chat!`)
     .addField(`:robot: Bot Created Date`, bot.user.createdAt)
-    .setFooter("Made by Corosive","https://cdn.discordapp.com/avatars/346579836292300800/b02ca29aee1ec133c84377bd4235e957.png")
     .setTimestamp();
 
     message.channel.send(botembed);
