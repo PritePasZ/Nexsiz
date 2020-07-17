434d31d62e657d4583b830e6be6c9f89a76097b2
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2020-07-17 16:46:56 +0700

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
   .setColor("#4286f4")
   .setAuthor(bname, bicon)
   .setURL("https://discordapp.com/oauth2/authorize?client_id=552076002101297153&scope=bot&permissions=2146954495")
-  .setDescription(":thumbsup::skin-tone-1: **Want to invite Nexsiz?, Just click at up there!**")
+  .setDescription(":thumbsup: **Want to invite Nexsiz?, Just click at up there!**")
   .setTitle(":arrow_right: Click here to invite Nexsiz in to your server! <:plus_1:557908827840970773>")
   .setFooter("Made By PriteToKung")
   .setTimestamp();
