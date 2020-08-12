a61699e976ab71ebbdb7a42ac3906198024c6730
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2020-08-12 13:03:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ fs.readdir("./commands/", (err, files) => {
 });
 
 // update here
-bot.user.setActivity(`with PriteToKung | n!help`);
+bot.user.setActivity('with PriteToKung', { type: 'WATCHING' })
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online, Bot is ready and working in ${bot.guilds.size} servers with ${bot.users.size} users!`);
   bot.user.setActivity(`with PriteToKung | n!help`);
