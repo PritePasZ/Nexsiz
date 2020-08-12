b6d704d9befe9150fd1d8d5d0a67cfd15bbaee50
PritePasZ <bassky2512@gmail.com>
2020-08-12 12:45:21 +0700

    VS Code test

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ fs.readdir("./commands/", (err, files) => {
   });
 
 });
-// update here - bot.user.setActivity(`with PriteToKung | n!help`, {type: "WATCHING"});
+// update here!! - bot.user.setActivity(`with PriteToKung | n!help`, {type: "WATCHING"});
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online, Bot is ready and working in ${bot.guilds.size} servers with ${bot.users.size} users!`);
   bot.user.setActivity(`with PriteToKung | n!help`);
