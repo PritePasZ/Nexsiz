02aa7d32069c9965fe6328b54ed3df9368735650
PritePasZ <bassky2512@gmail.com>
2020-08-16 15:25:55 +0700

    updated botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let version = `0.2.0a`;
+    let version = `0.2.6a`;
     let bicon = bot.user.displayAvatarURL;
     let bname = bot.user.username;
     let botembed = new Discord.RichEmbed()
@@ -14,7 +14,7 @@ module.exports.run = async (bot, message, args) => {
     .setDescription("Made with <:nodejs:553212483415048221> Node.js and <:discordjs:553213251488317440> Discord.js\nHosted bot for free by <:heroku:553414334156374016> Heroku")
     .addField(`:clipboard: Created by`, `PriteToKung#3071`, true)
     .addField(`:information_source: Version`, `${version}`, true)
-    .addField(`:tools: Debug`, `Updated to version ${version}, new commands - Kick & Ban commands in moderation category\n To invite Nexsiz in to your server! type "n!invite"\nCheck out try n!help in the chat!`)
+    .addField(`:tools: Debug`, `Updated to version ${version}, new commands,category - Miscellaneous Commands added.\n To invite Nexsiz in to your server! type "n!invite"\nCheck out try n!help in the chat!`)
     .addField(`:robot: Bot Created Date`, bot.user.createdAt)
     .setTimestamp();
 
