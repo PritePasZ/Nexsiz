657262fd6e25795d1d52f9eb44cbd5833a9c19ad
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2020-08-16 18:49:58 +0700

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -3,13 +3,13 @@ const Discord = require('discord.js')
 module.exports.run = async (bot, message, args) => {
 
   message.delete();
-  if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("**<:tickNo:576414524014329857> You dont have Administrator Permission!**").then(msg => {msg.delete(5000)})
-  if (!args[0]) return message.channel.send("<:tickNo:576414524014329857> Please type some question.");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("**<:tickNo:576414524014329857> ไม่มี Manage Messages Permission**").then(msg => {msg.delete(5000)})
+  if (!args[0]) return message.channel.send("<:tickNo:576414524014329857> โปรดใส่คำถาม");
   var question = args.join(" ");
   let pollEmbed = new Discord.RichEmbed()
   .setAuthor(message.author.username, message.author.displayAvatarURL)
   .setColor("#f44141")
-  .setTitle(":tada: Vote the message!")
+  .setTitle(":tada: Vote ข้อความ")
   .setDescription(args.join(' '))
 
   message.channel.send(pollEmbed).then(function (message) {
