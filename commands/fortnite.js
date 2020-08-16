d8361afb0097246e435c34913beb4a6a980d9008
PritePasZ <bassky2512@gmail.com>
2020-08-16 13:23:41 +0700

    created fortnite.js

---
+++ commands/fortnite.js
@@ -0,0 +1,35 @@
+const { RichEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
+const fortnite = require("simple-fortnite-api"), client = new fortnite("ad506295-e61a-4302-afe7-a0645c7bbfc6");
+
+module.exports.run = async (bot, message, args) => {
+        if(!args[0]) return message.channel.send("Please supply a username.");
+        if(args[1] && !["lifetime", "solo", "duo", "squad"].includes(args[1])) return message.channel.send("Usage: `!n!fortnite <username> <gametype>`\nGameTypes: Lifetime, Solo, Duo, Squad");
+        let gametype = args[1] ? args[1].toLowerCase() : "lifetime";
+
+        let data = await client.find(args[0])
+        if(data && data.code === 404) return message.channel.send(":x: Unable to find a user with that username.")
+            const { image, url, username } = data;
+            const { scorePerMin, winPercent, kills, score, wins, kd, matches } = data[gametype]
+
+                const embed = new RichEmbed()
+                    .setColor("#0080ff")
+                    .setAuthor(`Epic Games (Fortnite) | ${username}`, image)
+                    .setThumbnail(image)
+                    .setDescription(stripIndents`**:video_game: Gamemode:** ${gametype.slice(0, 1).toUpperCase() + gametype.slice(1)}
+                    **:skull: Kills:** ${kills || 0}
+                    **:clipboard: Score:** ${score || 0}
+                    **:alarm_clock: Score Per Min:** ${scorePerMin || 0}
+                    **:tada: Wins:** ${wins || 0}
+                    **:pencil: Win Ratio:** ${winPercent || "0%"}
+                    **:bar_chart: Kill/Death Ratio:** ${kd || 0}
+                    **:crossed_swords: Matches Played:** ${matches || 0}
+                    **:pushpin: Link:** [link to profile](${url})`)
+                    .setTimestamp()
+
+                    message.channel.send(embed)
+}
+
+module.exports.help = {
+    name: "fortnite"
+}
--- commands/xkcd.js
+++ commands/xkcd.js
@@ -3,12 +3,12 @@ const fetch = require("node-fetch");
 
 module.exports.run = async (bot, message, args) => {
 
-        if(!message.channel.nsfw) return message.channel.send("Please run this command in a `NSFW` channel.");
+        if(!message.channel.nsfw) return message.channel.send(":x: Please run this command in a `NSFW` channel.");
         if((args[1] && isNaN(args[1])) || !["search", "latest"].includes(args[0])) return message.channel.send("`n!xkcd` <search|latest> (id)");
         let search = args[1] ? `http://xkcd.com/${args[1]}/info.0.json` : "http://xkcd.com/info.0.json";
             try {
                 fetch(search).then(res => res.json()).then(res => {
-                    if(!res) return message.channel.send("No results found for this comic, sorry!");
+                    if(!res) return message.channel.send(":x: No results found for this comic, sorry!");
                     let { safe_title, img, day, month, year, num, alt} = res;
 
                     let xkcdembed = new RichEmbed()
