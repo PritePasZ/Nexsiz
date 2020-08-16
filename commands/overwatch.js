edfe998efec1ed1707b0f96dc27e6f7e54fde0cd
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2020-08-16 21:34:13 +0700

    Update overwatch.js

--- commands/overwatch.js
+++ commands/overwatch.js
@@ -43,7 +43,6 @@ module.exports.run = async (bot, message, args) => {
                     message.channel.send(embed);
                             
                 })
-
 }
 
 module.exports.help = {
