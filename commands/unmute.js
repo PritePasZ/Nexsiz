909fa20546ab6fe91042421102b3b37ec9daa5e6
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2020-08-16 19:16:46 +0700

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -2,16 +2,15 @@ module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.sendMessage("<:tickNo:576414524014329857> You don't have the Manage Messages premission")
 
         let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-        if(!toMute) return message.channel.sendMessage("<:tickNo:576414524014329857> Please mention an user or ID to mute!");
+        if(!toMute) return message.channel.sendMessage("<:tickNo:576414524014329857> โปรดแท็กผู้ใช้ หรือใส่ ID ผู้ใช้เพื่อปลด mute");
 
         let role = message.guild.roles.find(r => r.name === "Mute Role")
         
-        if(!role || !toMute.roles.has(role.id)) return message.channel.sendMessage("<:tickNo:576414524014329857> This user is not muted!");
+        if(!role || !toMute.roles.has(role.id)) return message.channel.sendMessage("<:tickNo:576414524014329857> ผู้ใช้นี้ยังไม่ถูก mute ไว้");
 
         await toMute.removeRole(role);
-        message.channel.sendMessage("The user has been unmuted! :pencil:");
+        message.channel.sendMessage("ผู้ใช้นี้ถูกปลด mute แล้ว :pencil:");
 
-        message.delete();
 
      }
     
