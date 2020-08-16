2c16c14da0161e7fc01effcc6111f462429ceba8
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2020-08-16 19:33:09 +0700

    Update steam.js

--- commands/steam.js
+++ commands/steam.js
@@ -5,11 +5,11 @@ const dateFormat = require("dateformat");
 
 module.exports.run = async (bot, message, args) => {
         const token = "5D55A206F400646275F4396C77D79E2B";
-        if(!args[0]) return message.channel.send(":x: Please provide an account name!");
+        if(!args[0]) return message.channel.send(":x: โปรดใส่ชื่อผู้ใช้");
         const url = `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${token}&vanityurl=${args.join(" ")}`;
 
         fetch(url).then(res => res.json()).then(body => {
-            if(body.response.success === 42) return message.channel.send("I was unable to find a steam profile with that name, `https://steamcommunity.com/id/(get this name)`");
+            if(body.response.success === 42) return message.channel.send("ไม่พบโปรไฟล์ Steam กับชื่อนี้, `https://steamcommunity.com/id/(เอาชื่อจากตรงนี้)`");
 
                 const id = body.response.steamid;
                 const summaries = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${token}&steamids=${id}`;
@@ -17,11 +17,11 @@ module.exports.run = async (bot, message, args) => {
                 const state = ["Offline", "Online", "Busy", "Away", "Snooze", "Looking to trade", "Looking to play"];
 
         fetch(summaries).then(res => res.json()).then(body => {
-            if(!body.response) return message.channel.send("I was unable to find a steam profile with that name, `https://steamcommunity.com/id/(get this name)`");
+            if(!body.response) return message.channel.send("ไม่พบโปรไฟล์ Steam กับชื่อนี้, `https://steamcommunity.com/id/(เอาชื่อจากตรงนี้)`");
             const { personaname, avatarfull, realname, personastate, loccountrycode, profileurl, timecreated } = body.response.players[0];
 
         fetch(bans).then(res => res.json()).then(body => {
-            if(!body.players) return message.channel.send("I was unable to find a steam profile with that name, `https://steamcommunity.com/id/(get this name)`");
+            if(!body.players) return message.channel.send(""ไม่พบโปรไฟล์ Steam กับชื่อนี้, `https://steamcommunity.com/id/(เอาชื่อจากตรงนี้)`");
             const { NumberOfVACBans, NumberOfGameBans} = body.players[0];
 
             const embed = new RichEmbed()
