466c71148d05a2c7f018eb215c54fbbd39196e23
PritePasZ <48234082+PritePasZ@users.noreply.github.com>
2020-08-17 07:15:34 +0700

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -16,7 +16,7 @@ module.exports.run = async (bot, message, args) => {
 		.setColor('#65f442')
 		.setThumbnail(user.avatarURL)
 		.setAuthor(`${user.username}`, user.avatarURL)
-    .addField("<:bustinsilhouette:553086092837126145> Username", `${user.username}#${user.discriminator}`,true)
+    .addField("<:bustinsilhouette:553086092837126145> ชื่อผู้ใช้", `${user.username}#${user.discriminator}`,true)
     .addField(`:card_index: ID ผู้ใช้`, `${user.id}`, true)
 		.addField(`:clipboard: ชื่อเล่น`, `${member.nickname !== null ? `${member.nickname}` : 'None'}`, true)
 		.addField(`<a:plexiOnline:478870259944783873> สถานะผู้ใช้`, `${user.presence.status}`, true)
